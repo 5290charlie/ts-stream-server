d4bfad0c763d285ee43ff70297f99d163aef8486
Charlie McClung <charlie@bowtie.co>
2022-07-28 19:41:46 -0600

    server updates

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,49 @@
-import fs from 'fs';
+import fs, { watch } from 'fs';
 import path from 'path';
-import net, { Socket } from 'net';
-import tls, { Server, TLSSocket, TLSSocketOptions } from 'tls';
+// import net, { Socket } from 'net';
+// import tls, { Server, TLSSocket, TLSSocketOptions } from 'tls';
 import ws, { WebSocketServer, WebSocket } from 'ws';
 import http, { IncomingMessage, ServerResponse as WebServerResponse } from 'http';
 import https, { Server as WebServer } from 'https';
 import express, { Express } from 'express';
 import session from 'express-session';
 import cors from 'cors';
-import socketio, { Server as SocketServer } from 'socket.io';
+import socketio, { Socket, Server as SocketServer } from 'socket.io';
+import { ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData } from './types';
 
 const { PORT = 8080 } = process.env;
 
 const app = express();
 const web = http.createServer(app);
-const io = new SocketServer(web, {
+const io = new SocketServer<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(web, {
   cors: {
     origin: 'http://localhost:3000',
     credentials: true
   }
 });
 
+let streamer: Socket | null = null;
+let watchers: { [id: string]: Socket } = {};
+
 io.on('connection', (socket) => {
-  console.log('Connected', socket);
+  console.log('Connected', socket.id);
+
+  socket.on('reg', (role) => {
+    if (role === 'streamer') {
+      console.log('Streaming', socket.id);
+      streamer = socket;
+    } else {
+      console.log('Watching', socket.id);
+      watchers[socket.id] = socket;
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Disconnect: ${socket.id}`);
+    if (watchers[socket.id]) {
+      delete watchers[socket.id];
+    }
+  });
 });
 
 web.listen(PORT, () => {
