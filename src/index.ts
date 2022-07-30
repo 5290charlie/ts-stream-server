bf935fad1e3d9345a0a3a46d239e6fabd25cbe6f
Charlie McClung <charlie@bowtie.co>
2022-07-29 20:28:22 -0600

    Better room usage

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import cors from 'cors';
 import socketio, { Socket, Server as SocketServer } from 'socket.io';
 import { ClientToServerEvents, ServerToClientEvents, InterServerEvents, RtcMsg } from './types';
 
-const { PORT = 8080, CORS_ORIGIN = 'http://localhost:3000' } = process.env;
+const { PORT = 8080, CORS_ORIGIN = 'http://localhost:3000', STREAM_ROOM = 'stream', GUEST_ROOM = 'guest', GLOBAL_ROOM = 'global' } = process.env;
 
 const app = express();
 const web = http.createServer(app);
@@ -26,6 +26,8 @@ let streamer: Socket | null = null;
 const clients: Socket[] = [];
 const watchers: { [id: string]: Socket } = {};
 
+io.socketsJoin(GLOBAL_ROOM);
+
 io.on('connection', (socket) => {
   console.log('Connected', socket.id);
 
@@ -35,27 +37,43 @@ io.on('connection', (socket) => {
     if (role === 'streamer') {
       console.log('Streaming', socket.id);
       streamer = socket;
+      socket.join(STREAM_ROOM);
     } else {
       console.log('Watching', socket.id);
+      socket.join(GUEST_ROOM);
       watchers[socket.id] = socket;
     }
   });
 
   socket.on('offer', (msg) => {
-    console.log('Offer', msg);
-    if (socket.id == streamer?.id) {
-
-    } else if (streamer) {
-      socket.to(streamer.id).emit('offer', msg);
-    }
+    console.log('Offer', socket.id, streamer?.id, msg.client);
+    socket.to(GUEST_ROOM).emit('offer', msg);
+    // if (socket.id == streamer?.id) {
+    //   socket.to(GUEST_ROOM).emit('offer', msg);
+    //   console.log("SEND TO GUEST_ROOM");
+    // } else if (streamer) {
+    //   socket.to(STREAM_ROOM).emit('offer', msg);
+    //   console.log("SEND TO STREAM_ROOM");
+    //   // socket.to(streamer.id).emit('offer', msg);
+    // }
   });
 
   socket.on('answer', (msg) => {
     console.log('Answer', msg);
+    if (socket.id == streamer?.id) {
+      socket.to(GUEST_ROOM).emit('answer', msg);
+    } else if (streamer) {
+      socket.to(STREAM_ROOM).emit('answer', msg);
+    }
   });
 
   socket.on('candidate', (msg) => {
     console.log('Candidate', msg);
+    if (socket.id == streamer?.id) {
+      socket.to(GUEST_ROOM).emit('candidate', msg);
+    } else if (streamer) {
+      socket.to(STREAM_ROOM).emit('candidate', msg);
+    }
   });
 
   socket.on('disconnect', () => {
